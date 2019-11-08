0498d7406396358ed883938324f897c23f338c54
Adam Miranda <adamballimiranda@mail.weber.edu>
2019-11-07 21:38:07 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myfinalproject/ProductsFragment.java
@@ -0,0 +1,31 @@
+package com.example.myfinalproject;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProductsFragment extends Fragment {
+
+
+    public ProductsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_products, container, false);
+    }
+
+}
